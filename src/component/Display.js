136b6a844006ee9a2e4d5273d9523e4866096fa2
AnushkaKanal-20 <60444506+AnushkaKanal-20@users.noreply.github.com>
2021-07-06 09:27:46 +0530

    Task-1-change-background-color

--- src/component/Display.js
+++ src/component/Display.js
@@ -4,14 +4,35 @@ import PropTypes from "prop-types";
 import "./Display.css";
 
 export default class Display extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { windowWidth: window.innerWidth };
+  }
+
+ handleResize = (e) => {
+  this.setState({ windowWidth: window.innerWidth }); // set state to width of the viewport
+ };
+
+ componentDidMount() {
+  window.addEventListener("resize", this.handleResize);
+ }
+
+ componentWillUnmount() {
+  window.addEventListener("resize", this.handleResize);
+ } 
+
   static propTypes = {
     value: PropTypes.string,
   };
 
+ 
   render() {
+    const { windowWidth } = this.state; 
+    const displayBackground = windowWidth < 500 ? "red" : "#858694"; // check if viewport width is less than 500
     return (
-      <div className="component-display">
-        <div>{this.props.value}</div>
+      <div className="component-display" style={{backgroundColor: displayBackground}}>
+        <div>{this.props.value} </div>
       </div>
     );
   }
